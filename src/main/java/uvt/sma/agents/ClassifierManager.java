8589c2c6cedecfbf305cf831ed5d9b4e7bc5e421
rainman226 <dota2romaniarobi@gmail.com>
2025-06-18 15:53:38 +0300

    fix: now Manager creates the required number of Workers

--- src/main/java/uvt/sma/agents/ClassifierManager.java
+++ src/main/java/uvt/sma/agents/ClassifierManager.java
@@ -8,6 +8,8 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,6 +18,7 @@ import java.util.Set;
 public class ClassifierManager extends Agent {
     private static final long serialVersionUID = 1L;
     private Set<String> scannedFiles = new java.util.HashSet<>();
+    private int maxFiles = 3; // maximum number of files allowed for each worker
     private static final Logger LOGGER = LogManager.getLogger(ClassifierManager.class);
     @Override
     protected void setup() {
@@ -71,6 +74,10 @@ public class ClassifierManager extends Agent {
                 // process the message content and adds the files to scannedFiles set
                 processMessageContent(content);
 
+                createWorkers();
+
+                //TODO answer back to the monitor
+
             } else {
                 block(); // Wait for the next message
             }
@@ -91,6 +98,41 @@ public class ClassifierManager extends Agent {
                 }
             }
             LOGGER.info("Processed scan request. Current scanned files: {}", scannedFiles.size());
+        }
+    }
+
+    private void createWorkers() {
+        try {
+            ContainerController container = getContainerController();
+
+            int workerCount = (int) Math.ceil((double) scannedFiles.size() / maxFiles);
+            LOGGER.info("Creating {} worker agents to process {} files.", workerCount, scannedFiles.size());
+
+            for(int i = 0; i < workerCount; i++) {
+                StringBuilder filesForWorker = new StringBuilder();
+                int filesAdded = 0;
+
+                for (String file : scannedFiles) {
+                    if (filesAdded < maxFiles) {
+                        if (filesForWorker.length() > 0) {
+                            filesForWorker.append(",");
+                        }
+                        filesForWorker.append(file);
+                        filesAdded++;
+                    } else {
+                        break;
+                    }
+                }
+                // Remove the files that were assigned to this worker from the scannedFiles set
+                scannedFiles.removeAll(Set.of(filesForWorker.toString().split(",")));
+
+                // Create the worker agent with the files assigned
+                Object[] args = new Object[]{filesForWorker.toString()};
+                AgentController workerAgent = container.createNewAgent("worker-" + (i + 1), ClassifierWorkerAgent.class.getName(), args);
+                workerAgent.start();
+            }
+        } catch (Exception e) {
+            LOGGER.error("Failed to create worker agents: {}", e.getMessage());
         }
     }
 
